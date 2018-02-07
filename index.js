1b1e38600a535391f21f87f06d659c3f43e6a57b
kalingwu@gmail.com <kalingwu@gmail.com>
2018-02-07 17:39:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,17 +29,10 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()
 
 /*
 
-
-  describe('add2(n)', function() {
-    it('adds two to n', function() {
-      const n = Math.floor(Math.random() * 1000)
-      expect(window.add2(n)).toEqual(n + 2)
-    })
-  })
 
   describe('funkyFunction()', function() {
     it('returns a function', function() {
